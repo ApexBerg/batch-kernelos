3a38840754594ad5d66820ca5c41e352e15fd1e7
ApexBerg <109145427+ApexBerg@users.noreply.github.com>
2022-08-19 15:56:06 +0800

    Add files via upload

--- kernelos.bat
+++ kernelos.bat
@@ -2,7 +2,7 @@
 ::            MIT LICENSED           ::
 ::           by NotInfinity          ::
 ::          aka Matthew Adams        ::
-::           Kernel OS v1.0          ::
+::           Kernel OS v1.1          ::
 :::::::::::::::::::::::::::::::::::::::
 :: SCROLL DOWN ::
 :::::::::::::::::
@@ -42,14 +42,70 @@ cls
 :: Var Start ::
 :::::::::::::::
 
-set "build=1.0.0"
+set "build=1.1.0"
 set "osbuild=2022"
-set "codename=Nite"
+set "codename=Nite2"
 set excom=null
 
 ::::::::::::::
 :: Var End  ::
 ::::::::::::::
+
+:::::::::::::::::::
+:: winver detect ::
+:::::::::::::::::::
+
+ver | find /i "Windows 2000" >nul
+if %errorlevel% == 0 set winver=Windows_2000
+
+ver | find /i "XP" > nul
+if %errorlevel% == 0 set winver=Windows_XP
+
+ver | find /i "version 6.0." >nul
+if %errorlevel% == 0 set winver=Windows_Vista
+
+ver | find /i "version 6.1." >nul
+if %errorlevel% == 0 set winver=Windows_7
+
+ver | find /i "version 6.2." >nul
+if %errorlevel% == 0 set winver=Windows_8
+
+ver | find /i "version 6.3." >nul
+if %errorlevel% == 0 set winver=Windows_8.1
+
+ver | find /i "version 10.0." >nul
+if %errorlevel% == 0 set winver=Windows_10
+
+ver | find /i "version 10.0.22000.739" >nul
+if %errorlevel% == 0 set winver=Windows_11
+
+if %winver%==Windows_XP goto :notsupportwin
+if %winver%==Windows_2000 goto :notsupportwin
+
+if %winver%==Windows_Vista set txtwinver=Windows Vista & goto pgosnew
+if %winver%==Windows_8.1 set txtwinver=Windows 8.1 & goto pgosnew
+if %winver%==Windows_8 set txtwinver=Windows 8 & goto pgosnew
+if %winver%==Windows_7 set txtwinver=Windows 7 & goto pgosnew
+if %winver%==Windows_10 set txtwinver=Windows 10 & goto pgosnew
+if %winver%==Windows_11 set txtwinver=Windows 11 & goto pgosnew
+
+:notsupportwin
+cls
+type data\playtxt.txt
+echo.
+echo.
+echo.
+echo Sorry your current windows version [%txtwinver%] is not compatible at the moment
+echo.
+echo [PRESS ANY KEY TO EXIT]
+pause >nul
+exit
+
+
+
+:::::::::::::::::::::::
+:: winver detect end ::
+:::::::::::::::::::::::
 
 
 
@@ -66,7 +122,7 @@ echo.
 echo.
 echo.
 echo.
-echo Checking Files
+echo Checking Russian Federation's Files
 msg "%username%" on the first time booting you will get bsod do not exit!
 ping localhost -n 2 >nul
 if not exist nano.exe==set excom=nano.exe & set stopcode=0x01 & goto BSOD
@@ -85,9 +141,10 @@ if not exist sounds\bsod.wav==set excom=bsod.wav & set stopcode=0x07 & goto BSOD
 
 if not exist sounds\alarm.wav==set excom=alarm.wav & set stopcode=0x08 & goto BSOD
 
-echo System Path: %cd%
+echo Path: %cd%
 ping localhost -n 3 >nul
-goto login
+if not exist password.ini==set exc=denied & goto regis
+if exist password.ini==set exc=pass & goto login1
 
 :BSOD
 cls
@@ -102,26 +159,14 @@ echo We're just collecting some error
 echo info, and then we'll launch
 echo automatic repair for you.
 echo.
+echo the bsod log has been saved at logs\logs.txt
+echo.
 echo what failed: %excom%
 echo stopcode: %stopcode%
 ping localhost -n 8 >nul
+cls
 echo Automatic Repair is in progress do not interrupt!
 call sysfail/recovery.bat
-
-:login
-cls
-echo.
-type data\playtxt.txt                          
-echo.
-echo.
-echo Login / Register
-echo.
-echo [1] Login
-echo [2] Register
-choice /c 12 /n
-if %errorlevel% == 1 goto login1
-if %errorlevel% == 2 goto regis
-goto login
 
 :regis
 cls
@@ -130,7 +175,8 @@ type data\playtxt.txt
 echo.
 echo.
 echo.
-set /p pass2=Password: 
+echo register.
+set /p pass2=Russia Federation's Password: 
 echo %pass2% > password.ini
 
 :login1
@@ -140,14 +186,15 @@ type data\playtxt.txt
 echo.
 echo.
 echo.
-set /p pass1= Password: 
+echo login.
+set /p pass1=Russia Federation's Password: 
 for /f "Delims=" %%a in (password.ini) do (
 
 set TEXT=%%a
 
 )
 
-if %pass1%==%text% goto pgos3
+if %pass1%==%text% goto pgos
 goto login2
 
 :login2
@@ -168,22 +215,6 @@ set TEXT=%%a
 if %pass2%==%text% goto pgos3
 goto login2
 
-:pgos3
-msg "%username%" open KEY.txt to get a license key
-cls
-echo.
-type data\playtxt.txt
-echo.
-echo.
-echo.
-set /p license= License Key: 
-if %license% == 8585-3528-2984-3963 goto pgos
-if %license% == 26732-29127-14006-30358 goto pgos
-if %license% == 13789-14981-26500-26706 goto pgos
-if %license% == 25715-29347-1483-729 goto pgos
-if %license% == 22794-23451-15598-13458 goto pgos
-goto pgos3
-
 
 :pgos1
 cls
@@ -197,24 +228,28 @@ goto pgos1
 
 
 :pgos
-msg "%username%" welcome to kernel os
 title Kernel OS
 cls
 echo.
 type data\playtxt.txt
 echo.
 echo.
+echo Hello boss this is our current military personnel
+echo Active 1,000,000 personnel
+echo Reserve 2,000,000 personnel
 echo.
 echo Current date: %date%
 echo Current time: %time%
 echo.
-set /p terminal=root@kernelos:~$ 
+set /p terminal=russia@federation:~$ 
 if %terminal% == notepad call nano.exe
 if %terminal% == help goto help
 if %terminal% == draw call data\draw\draw.bat
 if %terminal% == timer goto timer
 if %terminal% == time goto time
 if %terminal% == systeminfo goto sysinfo
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
 if %terminal% == vol goto vol
 if %terminal% == date goto date
 if %terminal% == calculator goto calc
@@ -243,7 +278,7 @@ if %ERRORLEVEL% == 12 nircmd setsysvolume 1 & sound play sounds\ding.wav
 goto sndset
 
 :help
-title Kernel OS
+title Kernel OS unhappy russia
 echo Showing Commands
 echo.
 echo help - shows all commands
@@ -253,13 +288,22 @@ echo date - shows current date
 echo calculator - math
 echo draw - basically paint.exe
 echo.
+echo ! danger area !
+echo.
+echo destroyua - destroy ukraine using our military
+echo sendnukeua - destroy ukraine using nukes
+echo.
+echo ! danger area !
+echo.
 echo controlpanel - unspecified
 echo slfdestruct - destroy os
 echo systeminfo - shows information about the system
 echo vol - displays a disk volume label and serial number
 echo ver - shows os info
-set /p terminal=root@kernelos:~$ 
+set /p terminal=russia@federation:~$ 
 if %terminal% == notepad call nano.exe
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
 if %terminal% == systeminfo goto sysinfo
 if %terminal% == help goto help
 if %terminal% == timer goto timer
@@ -283,13 +327,15 @@ pause >nul
 timeout %timer2%
 echo Playing alarm sound
 sound play sounds\alarm.wav
-set /p terminal=root@kernelos:~$ 
+set /p terminal=russia@federation:~$ 
 if %terminal% == notepad call nano.exe
 if %terminal% == help goto help
 if %terminal% == vol goto vol
 if %terminal% == timer goto timer
 if %terminal% == draw call data\draw\draw.bat
 if %terminal% == systeminfo goto sysinfo
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
 if %terminal% == time goto time
 if %terminal% == date goto date
 if %terminal% == calculator goto calc
@@ -305,15 +351,13 @@ echo.
 echo Kernel OS
 echo Version %build% (OS Build %osbuild%)
 echo Codename: %codename%
-echo (C) Not Company. All rights not reserved.
+echo Copyright (c) 2022 Matthew Adams
 echo.
 echo This product is licensed to:
 echo %USERNAME%
 echo.
-echo Your License Key:
-echo %license%
 echo.
-set /p terminal=root@kernelos:~$ 
+set /p terminal=russia@federation:~$ 
 if %terminal% == notepad call nano.exe
 if %terminal% == help goto help
 if %terminal% == timer goto timer
@@ -322,6 +366,8 @@ if %terminal% == date goto date
 if %terminal% == vol goto vol
 if %terminal% == draw call data\draw\draw.bat
 if %terminal% == calculator goto calc
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
 if %terminal% == controlpanel goto pgos1
 if %terminal% == systeminfo goto sysinfo
 if %terminal% == slfdestruct goto bsod1
@@ -333,11 +379,13 @@ title Kernel OS
 set /p MATH=root@playground:~/Math$  
 set /a result=%MATH%
 echo Result: %result%
-set /p terminal=root@kernelos:~$ 
+set /p terminal=russia@federation:~$ 
 if %terminal% == notepad call nano.exe
 if %terminal% == help goto help
 if %terminal% == vol goto vol
 if %terminal% == timer goto timer
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
 if %terminal% == time goto time
 if %terminal% == draw call data\draw\draw.bat
 if %terminal% == systeminfo goto sysinfo
@@ -350,19 +398,21 @@ goto calc
 
 :bsod1
 cls
-echo [%date%] Kernel OS has ran a problem with system.exe stopcode: SLF_DESTRUCT_INTIATED >> logs\logs.txt
+echo %date% Kernel OS has ran a problem with system.exe stopcode: N/A >> logs\logs.txt
 color c
 sound play sounds\bsod.wav
 echo.
 echo :(
 echo.
-echo Your device ran into a problem
+echo Russia Federation server has ran into a problem
 echo We're just collecting some error
 echo info, and then we'll launch
 echo automatic repair for you.
 echo.
-echo what failed: system
-echo stopcode: SLF_DESTRUCT_INTIATED
+echo bsod log has been saved at logs\logs.txt
+echo.
+echo what failed: terminal.exe
+echo stopcode: N/A
 echo.
 ping localhost -n 8 >nul
 echo Exiting
@@ -385,12 +435,14 @@ goto apps
 :time
 title Kernel OS
 echo Current time: %time%
-set /p terminal=root@kernelos:~$ 
+set /p terminal=russia@federation:~$ 
 if %terminal% == notepad call nano.exe
 if %terminal% == help goto help
 if %terminal% == timer goto timer
 if %terminal% == systeminfo goto sysinfo
 if %terminal% == time goto time
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
 if %terminal% == date goto date
 if %terminal% == draw call data\draw\draw.bat
 if %terminal% == calculator goto calc
@@ -403,10 +455,12 @@ goto time
 :date
 title Kernel OS
 echo Current date: %date%
-set /p terminal=root@kernelos:~$ 
+set /p terminal=russia@federation:~$ 
 if %terminal% == notepad call nano.exe
 if %terminal% == help goto help
 if %terminal% == timer goto timer
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
 if %terminal% == systeminfo goto sysinfo
 if %terminal% == time goto time
 if %terminal% == vol goto vol
@@ -421,7 +475,7 @@ goto date
 :vol
 title Kernel OS
 vol
-set /p terminal=root@kernelos:~$ 
+set /p terminal=russia@federation:~$ 
 if %terminal% == notepad call nano.exe
 if %terminal% == systeminfo goto sysinfo
 if %terminal% == help goto help
@@ -430,6 +484,8 @@ if %terminal% == time goto time
 if %terminal% == vol goto vol
 if %terminal% == draw call data\draw\draw.bat
 if %terminal% == date goto date
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
 if %terminal% == calculator goto calc
 if %terminal% == controlpanel goto pgos1
 if %terminal% == slfdestruct goto bsod1
@@ -452,7 +508,7 @@ systeminfo | findstr /C:"Boot Device"
 systeminfo | findstr /C:"System Locale"
 systeminfo | findstr /C:"Input Locale"
 systeminfo | findstr /C:"Time Zone"
-set /p terminal=root@kernelos:~$ 
+set /p terminal=russia@federation:~$ 
 if %terminal% == notepad call nano.exe
 if %terminal% == help goto help
 if %terminal% == timer goto timer
@@ -460,9 +516,58 @@ if %terminal% == systeminfo goto sysinfo
 if %terminal% == time goto time
 if %terminal% == draw call data\draw\draw.bat
 if %terminal% == vol goto vol
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
 if %terminal% == date goto date
 if %terminal% == calculator goto calc
 if %terminal% == controlpanel goto pgos1
 if %terminal% == slfdestruct goto bsod1
 if %terminal% == ver goto ver
 goto vol
+
+:desua
+echo Capital can be anything
+set /p capital=Capital: 
+echo Destroying %capital% on UKRAINE with %random% personnel
+ping localhost -n 8 > nul
+echo UKRAINE Lost we won.
+set /p terminal=russia@federation:~$ 
+if %terminal% == notepad call nano.exe
+if %terminal% == help goto help
+if %terminal% == timer goto timer
+if %terminal% == systeminfo goto sysinfo
+if %terminal% == time goto time
+if %terminal% == draw call data\draw\draw.bat
+if %terminal% == vol goto vol
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
+if %terminal% == date goto date
+if %terminal% == calculator goto calc
+if %terminal% == controlpanel goto pgos1
+if %terminal% == slfdestruct goto bsod1
+if %terminal% == ver goto ver
+goto desua
+
+:sndnukeua
+echo Capital can be anything.
+set /p capital=Capital: 
+set /a num=%rndnum% %%100 +1
+echo Destroying %capital% on UKRAINE with %rndnum% nukes
+ping localhost -n 6 > nul
+echo UKRAINE Lost we won.
+set /p terminal=russia@federation:~$ 
+if %terminal% == notepad call nano.exe
+if %terminal% == help goto help
+if %terminal% == timer goto timer
+if %terminal% == systeminfo goto sysinfo
+if %terminal% == time goto time
+if %terminal% == draw call data\draw\draw.bat
+if %terminal% == vol goto vol
+if %terminal% == destroyua goto desua
+if %terminal% == sendnukeua goto sndnukeua
+if %terminal% == date goto date
+if %terminal% == calculator goto calc
+if %terminal% == controlpanel goto pgos1
+if %terminal% == slfdestruct goto bsod1
+if %terminal% == ver goto ver
+goto sndnukeua
